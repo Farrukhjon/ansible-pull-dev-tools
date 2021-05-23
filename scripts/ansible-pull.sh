8d4fe0b3d6fb9adb18dd860de9c8056ce4336d07
FSattorov <farrukhjon.sattorov@gmail.com>
2021-05-23 22:25:33 +0300

    added ansible-pull script

---
+++ scripts/ansible-pull.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if ! command -v git &>/dev/null; then
+  echo "git was not found, trying to install it"
+  sudo dnf -y update && sudo dnf -y install git
+fi
+
+if ! command -v ansible-pull &>/dev/null; then
+  echo "ansible was not found, trying to install it"
+  sudo dnf install ansible
+fi
+
+ansible-pull --checkout=main \
+  --inventory=localhost, \
+  --module-name=git \
+  --url="https://github.com/Farrukhjon/ansible-pull-dev-tools.git" \
+  --directory /tmp/ansible/ansible-pull \
+  --user vagrant \
+  --only-if-changed \
+  --verbose \
+  playbook.yml
